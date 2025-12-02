8031563fcd6aecb286bc6bcfa4207d0d1dd190a5
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2025-12-02 16:44:05 +0100

    Update highlights.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,21 +1,32 @@
+; ——— Identifiers & Annexes
 (identifier) @variable
+;
+; highlight annexes as builtins, which probably makes sense
+(annex
+  ; also highlight aliases
+  (identifier)? @variable.builtin
+  alias: (identifier)? @variable.other
+  normalizer: (identifier)? @variable.other
+) @variable.builtin
 
+; ——— Functions & Parameters ———
+;
 (lam
   name: (identifier) @variable.function
   (pattern (identifier) @variable.parameter)*
   (pattern
     (pattern .(identifier) @variable.parameter))*
   (pattern (group (identifier) @variable.parameter))*)
-
+(function_type
+  (pattern (pattern (identifier) @variable.parameter)))
+(function_type
+  (pattern (group (identifier) @variable.parameter)))
+;
+; highlight identifiers used as functions
 (application . (identifier) @variable.function (_))
 
-(annex (identifier)? @variable.builtin
-  alias: (identifier)? @variable.other
-  normalizer: (identifier)? @variable.other
-) @variable.builtin
-
-(_ type: (identifier) @type)
-
+; ——— Brackets ———
+;
 [
   "("
   ")"
@@ -34,12 +45,16 @@
   ">"
 ] @punctuation.bracket
 
+; ——— Delimiters ———
+;
 [
   ","
   ";"
   "."
 ] @punctuation.delimiter
 
+; ——— Operators ———
+;
 [
   "="
   "#"
@@ -49,34 +64,33 @@
   "→"
 ] @operator
 
+; ——— Keywords ———
+;
 [
   "import"
   "plugin"
 ] @keyword.control.import
-
+;
 [
   "let"
   "axm"
 ] @keyword.storage.type
-
+;
 [
   "lam"
   "con"
   "fun"
   "Sigma"
 ] @keyword.function
-
+;
 [
   "where"
   "end"
   "extern"
 ] @keyword
 
-[
-  "tt"
-  "ff"
-] @constant.builtin
-
+; ——— Types
+;
 [
   "Cn"
   "Fn"
@@ -90,16 +104,39 @@
   "⊥" ".bot"
   "⊤" ".top"
 ] @type.builtin
+;
+; highlight identifiers that appear where types or type level functions
+; are expected (with up to eight parameters)
+(_ type: (identifier) @type)
+(_ type: (application (identifier) @type (_)))
+(_ type: (application (application (identifier) @type (_)) (_)))
+(_ type: (application (application (application (identifier) @type (_)) (_)) (_)))
+(_ type: (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)))
+(_ type: (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)))
+(_ type: (application (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)) (_)))
+(_ type: (application (application (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)) (_)) (_)))
+(_ type: (application (application (application (application (application (application (application (application (identifier) @type (_)) (_)) (_)) (_)) (_)) (_)) (_)) (_)))
 
+"return" @keyword.control
 
+; ——— Literals ———
+;
 (int_literal) @constant.numeric
-
+;
 (char_literal) @constant.character
-
+;
 (string_literal) @string
+;
+[
+  "tt"
+  "ff"
+] @constant.builtin
 
+; ——— Comments ———
+; doc comment markdown injection is handled in injections.scm
+;
 (line_comment) @comment.line
-
+;
 (block_comment) @comment.block
-
+;
 (doc_comment) @comment.line.documentation
